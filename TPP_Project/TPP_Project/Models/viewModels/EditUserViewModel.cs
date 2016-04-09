68e4182dba7b853ee825fc21ef0dc0b944a0d132
Рома Цімура <rio1995@meta.ua>
2016-04-09 23:52:24 +0300

    added user's view  model

---
+++ TPP_Project/TPP_Project/Models/viewModels/EditUserViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using TPP_Project.Models.entities;
+
+namespace TPP_Project.Models.ViewModels
+{
+    public class EditUserViewModel
+    {
+
+        [Required]
+        public string FistName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Organization { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Role { get; set; }
+        public string Id { get; set; }
+
+
+        public EditUserViewModel() { }
+        public EditUserViewModel(ApplicationUser user)
+        {
+            this.FistName = user.FistName;
+            this.LastName = user.LastName;
+            this.Organization = user.Organization;
+            this.City = user.City;
+            this.Country = user.Country;
+            this.Role = user.RoleName;
+            this.Id = user.Id;
+        }
+    }
+}
