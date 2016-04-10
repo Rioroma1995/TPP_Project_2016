8e773142fc97bc5fd55ef3044346a4732ce7faad
slavaUkraini <krainik.vita@gmail.com>
2016-04-10 18:39:21 +0300

    Home page
    fucking home page

--- TPP_Project/TPP_Project/App_Start/BundleConfig.cs
+++ TPP_Project/TPP_Project/App_Start/BundleConfig.cs
@@ -23,7 +23,15 @@ namespace TPP_Project
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/site.css",
+                        "~/Content/bootstrap.css",
+                        "~/Content/bootstrap.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/respond.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
